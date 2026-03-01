be34ad60759516dc525de968ac70b22556f79ff7
rlaxogh5079 <rlaxogh507906@gmail.com>
2026-03-02 04:41:20 +0900

    :heavy_plus_sign: Add TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,12 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./App.tsx";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const rootElement = document.getElementById("root");
+if (!rootElement) throw new Error("Failed to find the root element");
+
+const root = ReactDOM.createRoot(rootElement);
 root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
